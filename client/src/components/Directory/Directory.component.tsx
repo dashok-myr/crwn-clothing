bba278d2094260daca12046a84fbd051e9f6fad0
Daria Usatyuk <dashok.myr@gmail.com>
2021-01-19 21:06:34 -0800

    converted directory to typescript

--- client/src/components/Directory/Directory.component.tsx
+++ client/src/components/Directory/Directory.component.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import MenuItem from "../menu-item/MenuItem.component";
 import "./directory.styles.scss";
 import { useSelector } from "react-redux";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
-const Directory = () => {
-  const sections = useSelector(selectDirectorySections);
+export interface ISection {
+  title: string;
+  imageUrl: string;
+  id: number;
+  linkUrl: string;
+}
 
+const Directory = (): ReactElement => {
+  const sections: ISection[] = useSelector(selectDirectorySections);
   return (
     <div className="directory-menu">
       {sections.map(({ id, imageUrl, linkUrl, title }) => {
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Directory from "../../components/directory/directory.component";
+import Directory from "../../components/Directory/Directory.component";
 import { HomePageStyles } from "./homepage.styles";
 
 const HomePage = () => {
